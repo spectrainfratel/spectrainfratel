fecdd080dc5d2e3ecb8e047e5df62fcca2ca0b0a
Susanta Biswas <susantabiswas8116@gmail.com>
2025-10-17 19:20:22 +0530

    Fixed the margin issue in services

--- src/app/home/components/services.jsx
+++ src/app/home/components/services.jsx
@@ -26,9 +26,9 @@ export default function Services() {
                     </Carousel>
                 </div>
             </div>
-            <div className="service-bottom w-full h-[300px] sm:h-[400px] md:h-[500px] text-white bg-[#001020] flex items-end pe-5 sm:pe-7 lg:mt-[250px] ">
-                <div className="flex justify-end items-center border-r-4 sm:border-r-8 [border-image:linear-gradient(135deg,_rgba(2,197,253,1),_rgba(13,80,167,1))_1] w-full max-h-fit mb-20 sm:mb-30">
-                    <p className='pe-5 sm:pe-7 text-xs sm:text-sm md:text-base lg:text-lg text-gray-600 italic text-right w-2/3 sm:w-1/3'>
+            <div className="service-bottom w-full sm:h-[500px] md:h-[500px] text-white bg-[#001020] flex items-end pe-5 sm:pe-7 lg:mt-[-330px]">
+                <div className="flex justify-end items-center border-r-4 sm:border-r-8 [border-image:linear-gradient(135deg,_rgba(2,197,253,1),_rgba(13,80,167,1))_1] w-full max-h-fit my-5">
+                    <p className='pe-5  sm:pe-7 text-xs sm:text-sm md:text-base lg:text-lg text-gray-600 italic text-right w-2/3 sm:w-1/3'>
                         Spectra Infratel provides end-to-end, Turnkey Telecommunications Infrastructure services, which means they handle the entire process of building and maintaining mobile and fiber optic networks for carriers and large enterprises.
                     </p>
                 </div>
